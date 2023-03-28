630418a53e88d40754275d861343d15ab888df8b
kolade <chrisjay967@gmail.com>
2023-03-28 11:29:30 +0100

    triggering conflicts from main

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
-function addNums(a, b) {
+function addNumbers(a, b) {
   return a + b;
 }
 
 console.log(addNums(2, 8));
 
-// New feature branch
+function subtractNums(a, b) {
+  return a - b;
+  // I cant reach this line
+}
+
+console.log(subtractNums(8, 2));
